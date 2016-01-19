e9d9e69ede49c7adfc52d61207e77846205c98e7
centilmen50 <zorolum00@hotmail.com>
2016-01-19 04:14:09 +0200

    update

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ using System.Drawing;
 
 namespace PerfectGraves
 {
+
     class Program
     {
 
@@ -26,16 +27,21 @@ namespace PerfectGraves
             get { return ObjectManager.Player; }
         }
 
-        public static Menu menu, ComboMenu, DrawingsMenu, FarmMenu, HarassMenu, UpdateMenu, KSMenu;
+        public static Menu menu, ComboMenu, DrawingsMenu, FarmMenu, HarassMenu, UpdateMenu, KSMenu, MiscMenu;
         public static Spell.Skillshot Q;
         public static Spell.Skillshot W;
         public static Spell.Skillshot E;
         public static Spell.Skillshot R;
         public static Spell.Skillshot R1;
         private static Vector3 mousePos { get { return Game.CursorPos; } }
+        public static float Etime { get; set; }
 
         private static void Loading_OnLoadingComplete(EventArgs args)
         {
+            if (Player.Instance.ChampionName != "Graves")
+            {
+                return;
+            }
             Bootstrap.Init(null);
             Q = new Spell.Skillshot(SpellSlot.Q, 950, SkillShotType.Linear, 250, 2000, 60);
             W = new Spell.Skillshot(SpellSlot.W, 850, SkillShotType.Circular, 250, 1650, 150);
@@ -46,11 +52,13 @@ namespace PerfectGraves
             menu = MainMenu.AddMenu("Perfect Graves", "PerfectGraves");
 
             ComboMenu = menu.AddSubMenu("Combo Settings", "Combo");
+            ComboMenu.Add("disableAA", new CheckBox("Disable AA"));
             ComboMenu.Add("useQCombo", new CheckBox("Use Q"));
             ComboMenu.Add("useWCombo", new CheckBox("Use W"));
             ComboMenu.Add("useECombo", new CheckBox("Use E"));
             ComboMenu.Add("useRCombo", new CheckBox("Fast R Combo"));
-            ComboMenu.Add("useItems", new CheckBox("Use Items"));
+            ComboMenu.Add("useItems", new CheckBox("Use Items")); 
+            ComboMenu.Add("useEreload", new CheckBox("Use E for Reload"));
             ComboMenu.AddLabel("BOTRK,Bilgewater Cutlass Settings");
             ComboMenu.Add("botrkHP", new Slider("My HP < %", 60, 0, 100));
             ComboMenu.Add("botrkenemyHP", new Slider("Enemy HP < %", 60, 0, 100));
@@ -73,21 +81,16 @@ namespace PerfectGraves
             FarmMenu.Add("Ejungle", new CheckBox("Use E"));
             FarmMenu.Add("EjungleMana", new Slider("Mana < %", 45, 0, 100));
 
+            MiscMenu = menu.AddSubMenu("More Settings", "MoreSettings");
+            MiscMenu.Add("gapcloserE", new CheckBox("Use E Gapcloser"));
+            MiscMenu.Add("gapcloserW", new CheckBox("Use W Gapcloser"));
+
             DrawingsMenu = menu.AddSubMenu("Draw Settings", "Drawings");
             DrawingsMenu.Add("DrawQ", new CheckBox("Draw Q"));
             DrawingsMenu.Add("DrawW", new CheckBox("Draw W"));
             DrawingsMenu.Add("DrawE", new CheckBox("Draw E"));
             DrawingsMenu.Add("DrawR", new CheckBox("Draw R"));
             DrawingsMenu.Add("DrawR1", new CheckBox("Draw Extended R"));
-
-            UpdateMenu = menu.AddSubMenu("Updates", "Update");
-            UpdateMenu.AddLabel("0.0.3 Updated");
-            UpdateMenu.AddLabel("+Jungle Clear Added!");
-            UpdateMenu.AddLabel("+Kill Steal Added!");
-            UpdateMenu.AddLabel("+Cast Q Improwed!");
-            UpdateMenu.AddLabel("-Lane Clear Closed for Now.");
-
-
 
             Game.OnTick += Game_OnTick;
             Drawing.OnDraw += Drawing_OnDraw;
@@ -98,9 +101,15 @@ namespace PerfectGraves
 
             Orbwalker.ForcedTarget = null;
             if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+            {
                 Combo();
+            }
+
             if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear))
+            {
                 LaneClear();
+            }
+
             if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear))
             {
                 JungleClear();
@@ -108,6 +117,221 @@ namespace PerfectGraves
             KillSteal();
 
         }
+
+        public static void OnPostAttack(AttackableUnit target, EventArgs args)
+        {
+            if (_Player.HasBuff("GravesBasicAttackAmmo2") || !E.IsReady() ||
+                !ComboMenu["useEreload"].Cast<CheckBox>().CurrentValue ||
+                !(Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear) ||
+                Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo)))
+                return;
+            var direction = (Game.CursorPos - _Player.ServerPosition).To2D().Normalized();
+
+            for (var step = 0f; step < 360; step += 30)
+            {
+                for (var a = 450; a > 0; a -= 50)
+                {
+                    var currentAngle = step * (float)Math.PI / 90;
+                    var currentCheckPoint = _Player.ServerPosition.To2D() +
+                                            a * direction.Rotated(currentAngle);
+
+                    if (NavMesh.GetCollisionFlags(currentCheckPoint).HasFlag(CollisionFlags.Wall) ||
+                        NavMesh.GetCollisionFlags(currentCheckPoint).HasFlag(CollisionFlags.Building))
+                        continue;
+                    {
+                        E.Cast((Vector3)currentCheckPoint);
+                    }
+                }
+            }
+        }
+
+        public static void OnGapCloser(AIHeroClient sender, Gapcloser.GapcloserEventArgs e)
+        {
+            if (!sender.IsValidTarget() || sender.IsAlly) return;
+
+            if (MiscMenu["gapcloserE"].Cast<CheckBox>().CurrentValue)
+            {
+                var direction = (e.End + _Player.ServerPosition).To2D().Normalized();
+
+                for (var step = 0f; step < 360; step += 30)
+                {
+                    for (var a = 200; a < 450; a += 50)
+                    {
+                        var currentAngle = step * (float)Math.PI / 90;
+                        var currentCheckPoint = _Player.ServerPosition.To2D() +
+                                                a * direction.Rotated(currentAngle);
+
+                        if (!IsSafePosition((Vector3)currentCheckPoint) ||
+                            NavMesh.GetCollisionFlags(currentCheckPoint).HasFlag(CollisionFlags.Wall) ||
+                            NavMesh.GetCollisionFlags(currentCheckPoint).HasFlag(CollisionFlags.Building))
+                            continue;
+                        {
+                            E.Cast((Vector3)currentCheckPoint);
+                        }
+                    }
+                }
+            }
+
+            if (MiscMenu["gapcloserW"].Cast<CheckBox>().CurrentValue &&
+                W.IsReady() && _Player.Distance(e.End) <= W.Range)
+                W.Cast(e.End);
+        }
+        public static bool UnderAllyTurret(Vector3 pos)
+        {
+            return ObjectManager.Get<Obj_AI_Turret>().Any(t => t.IsAlly && !t.IsDead && pos.Distance(t) <= 900);
+        }
+
+        public static bool UnderEnemyTurret(Vector3 pos)
+        {
+            return ObjectManager.Get<Obj_AI_Turret>().Any(t => t.IsEnemy && !t.IsDead && pos.Distance(t) <= 900);
+        }
+
+        public static bool InSpawnPoint(Vector3 pos)
+        {
+            return ObjectManager.Get<Obj_SpawnPoint>().Any(x => pos.Distance(x) < 800 && x.IsEnemy);
+        }
+
+        public static int CountAlliesInRange(Vector3 pos)
+        {
+            var allies = EntityManager.Heroes.Allies.Count(
+                allied => !allied.IsDead && allied.Distance(pos) <= 800);
+            return allies;
+        }
+        public static List<AIHeroClient> GetLowaiAiHeroClients(Vector3 position, float range)
+        {
+            return
+                EntityManager.Heroes.Enemies.Where(
+                    hero => hero.IsValidTarget() && (hero.Distance(position) <= range) && hero.HealthPercent <= 15)
+                    .ToList();
+        }
+
+        public static void OnPreAttack(GameObject target, EventArgs args)
+        {
+            if (_Player.HasBuff("GravesBasicAttackAmmo2") || _Player.HasBuff("GravesBasicAttackAmmo1") ||
+                !Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo) ||
+                target.Type != GameObjectType.AIHeroClient || !ComboMenu["disableAA"].Cast<CheckBox>().CurrentValue)
+                return;
+
+            if (Prediction.Position.Collision.LinearMissileCollision(
+                target as Obj_AI_Base, _Player.ServerPosition.To2D(), target.Position.To2D(), int.MaxValue,
+                (int)target.BoundingRadius, 0))
+            {
+                Orbwalker.DisableAttacking = true;
+            }
+        }
+
+        public static void OnSpellCast(GameObject sender, GameObjectProcessSpellCastEventArgs args)
+        {
+            if (sender.IsDead || !sender.IsMe) return;
+
+            if (args.Slot == SpellSlot.Q)
+                Orbwalker.ResetAutoAttack();
+
+            if (args.Slot == SpellSlot.E)
+                Orbwalker.ResetAutoAttack();
+
+            if (args.Slot == SpellSlot.R)
+                Orbwalker.ResetAutoAttack();
+        }
+
+        public static Vector3 WallQpos(AIHeroClient target)
+        {
+            if (target == null)
+            {
+                return new Vector3();
+            }
+
+            for (var i = 100; i < 900; i += 100)
+            {
+                var qPos = new Vector3(
+                    Prediction.Position.PredictUnitPosition(target, 250).To3D().X + i,
+                    Prediction.Position.PredictUnitPosition(target, 250).To3D().Y + i,
+                    Prediction.Position.PredictUnitPosition(target, 250).To3D().Z);
+                if (
+                    NavMesh.GetCollisionFlags(qPos.To2D()
+                        .RotateAroundPoint((Vector2)qPos, 20)).HasFlag(CollisionFlags.Building) ||
+                    NavMesh.GetCollisionFlags(qPos.To2D()
+                        .RotateAroundPoint((Vector2)qPos, 20)).HasFlag(CollisionFlags.Wall))
+                    return qPos;
+            }
+            return new Vector3();
+        }
+
+        public static Vector3 DashtoQpos(AIHeroClient target)
+        {
+            if (target == null)
+            {
+                return new Vector3();
+            }
+
+            if (!WallQpos(target).IsValid()) return new Vector3();
+
+            for (var p = 300; p < 900; p += 100)
+            {
+                var ePos = WallQpos(target).Extend(target.Position, target.Distance(WallQpos(target)) + p).To3D();
+                if (ePos.Distance(_Player.ServerPosition) <= 300 &&
+                    !(NavMesh.GetCollisionFlags(ePos).HasFlag(CollisionFlags.Building) ||
+                      NavMesh.GetCollisionFlags(ePos).HasFlag(CollisionFlags.Wall)))
+                {
+                    return ePos;
+                }
+            }
+            return new Vector3();
+        }
+
+        public static bool IsInGrass(AIHeroClient unit)
+        {
+            return unit.IsValidTarget(900) &&
+                   (NavMesh.GetCollisionFlags(unit.ServerPosition).HasFlag(CollisionFlags.Grass) ||
+                    NavMesh.GetCollisionFlags(Prediction.Position.PredictUnitPosition(unit, 500))
+                        .HasFlag(CollisionFlags.Grass));
+        }
+        public static bool IsSafePosition(Vector3 position)
+        {
+            var enemies = position.CountEnemiesInRange(800);
+            var allies = CountAlliesInRange(position);
+            var turrets = EntityManager.Turrets.Allies.Count(x => _Player.Distance(x) < 800 && x.IsValid && !x.IsDead);
+            var lowEnemies = GetLowaiAiHeroClients(position, 800).Count();
+
+            if (UnderEnemyTurret(position)) return false;
+
+            if (enemies == 1)
+            {
+                return true;
+            }
+            return allies + turrets > enemies - lowEnemies;
+        }
+
+        public static void CastCollisionQ(AIHeroClient target)
+        {
+            for (var i = 50; i < Q.Range - _Player.Distance(target.ServerPosition); i += 50)
+            {
+                var predPosQ = Prediction.Position.PredictLinearMissile(target, Q.Range - 100, Q.Width, Q.CastDelay,
+                    Q.Speed, int.MaxValue, null);
+                var predPosT = predPosQ.CastPosition
+                    .Extend(_Player.ServerPosition.To2D(), -i).To3D();
+
+                if (!NavMesh.GetCollisionFlags(predPosT).HasFlag(CollisionFlags.Wall) &&
+                    !NavMesh.GetCollisionFlags(predPosT).HasFlag(CollisionFlags.Building) ||
+                    _Player.Distance(predPosT) > Q.Range)
+                    continue;
+
+                Q.Cast((Vector3)_Player.ServerPosition.Extend(predPosT, Q.Range));
+            }
+        }
+
+        public static void CastQkill(AIHeroClient target)
+        {
+            var predPos = Prediction.Position.PredictLinearMissile(target, Q.Range - 100, Q.Width, Q.CastDelay, Q.Speed,
+                int.MaxValue, null);
+
+            if (predPos.HitChance >= HitChance.Medium && Game.Time > Etime + 250 &&
+                target.Health + 20 < _Player.GetSpellDamage(target, SpellSlot.Q))
+            {
+                Q.Cast(predPos.CastPosition);
+            }
+        }
+
         public static void KillSteal()
         {
             if (Orbwalker.IsAutoAttacking) return;
@@ -127,6 +351,18 @@ namespace PerfectGraves
             if (useQ && Q.IsReady() && targetQ.IsValidTarget(Q.Range) && targetQ.Health < QDamage(targetQ) && Q.GetPrediction(targetQ).HitChance >= HitChance.Medium)
             {
                 Q.Cast(Q.GetPrediction(targetQ).CastPosition);
+            }
+        }
+
+        public static void CastWslow(AIHeroClient target)
+        {
+            var predPos = Prediction.Position.PredictCircularMissile(target, W.Range, W.Width, W.CastDelay, W.Speed,
+                null, true);
+
+            if (predPos.HitChance >= HitChance.Medium &&
+                target.HealthPercent < 20)
+            {
+                Q.Cast(predPos.CastPosition);
             }
         }
 
@@ -194,7 +430,6 @@ namespace PerfectGraves
         }
         public static void Combo()
         {
-            if (Orbwalker.IsAutoAttacking) return;
             var UseItems = ComboMenu["useItems"].Cast<CheckBox>().CurrentValue;
             var useQ = ComboMenu["useQCombo"].Cast<CheckBox>().CurrentValue;
             var useW = ComboMenu["useWCombo"].Cast<CheckBox>().CurrentValue;
@@ -204,7 +439,8 @@ namespace PerfectGraves
             var targetR = TargetSelector.GetTarget(R.Range, DamageType.Physical);
             var targetR1 = TargetSelector.GetTarget(R1.Range, DamageType.Physical);
             var targetQ = TargetSelector.GetTarget(Q.Range, DamageType.Physical);
-            if (!Player.HasBuff("gravesbasicattackammo2"))
+            var tHp = targetQ.Health + 20;
+            if (!Player.HasBuff("gravesbasicattackammo2") && !Orbwalker.IsAutoAttacking)
             {
                 if (useE && E.IsReady() && targetE.IsValidTarget(Q.Range))
                 {
@@ -215,9 +451,13 @@ namespace PerfectGraves
             {
                 R.Cast(R.GetPrediction(targetR).CastPosition);
             }
-            if (useQ && Q.IsReady() && Q.GetPrediction(targetQ).HitChance >= HitChance.Medium)
+            if (useQ && Q.IsReady() && Q.GetPrediction(targetQ).HitChance >= HitChance.Medium && tHp < _Player.GetSpellDamage(targetQ, SpellSlot.Q))
             {
-                Q.Cast(Q.GetPrediction(targetQ).CastPosition);
+                CastQkill(targetQ);
+            }
+            else
+            {
+                CastCollisionQ(targetQ);
             }
             foreach (var target in HeroManager.Enemies.Where(o => o.IsValidTarget(1300) && !o.IsDead && !o.IsZombie))
             {
@@ -233,14 +473,7 @@ namespace PerfectGraves
         }
         public static void LaneClear()
         {
-            /*
-            var minions = ObjectManager.Get<Obj_AI_Minion>().Where(m => m.IsValidTarget(Q.Range));
-            var useQ = FarmMenu["useQ"].Cast<CheckBox>().CurrentValue;
-            if (useQ && Q.IsReady() && Q.IsInRange(minions.FirstOrDefault().Position) && Q.GetPrediction(minions.FirstOrDefault()).HitChance >= HitChance.High)
-            {
-                //Q.Cast(minions.First().Position);
-            }
-            */
+
         }
 
         private static void JungleClear()
@@ -250,7 +483,7 @@ namespace PerfectGraves
             var useQMana = FarmMenu["QjungleMana"].Cast<Slider>().CurrentValue;
             var useE = FarmMenu["Ejungle"].Cast<CheckBox>().CurrentValue;
             var useEMana = FarmMenu["EjungleMana"].Cast<Slider>().CurrentValue;
-                foreach (var monster in EntityManager.MinionsAndMonsters.Monsters)
+            foreach (var monster in EntityManager.MinionsAndMonsters.Monsters)
             {
                 if (useQ && Q.IsReady() && Player.Instance.ManaPercent > useQMana)
                 {
@@ -264,6 +497,7 @@ namespace PerfectGraves
                 HandleItems();
             }
         }
+
 
         private static void Drawing_OnDraw(EventArgs args)
         {
